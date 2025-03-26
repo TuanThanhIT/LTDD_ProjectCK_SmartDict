9bfb6ae89c7fd3da11bde18ed6a33da4d39ded9d
“LucAnL2” <huynhthaitoan.12c1htk@gmail.com>
2025-03-26 23:31:59 +0700

    update code 26/03/2025

---
+++ Project_LTDD/app/src/main/java/com/example/project_ltdd/activities/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.project_ltdd.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.project_ltdd.R;
+
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
